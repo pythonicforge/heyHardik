2f34d6feec59bcd511494d2c536bdccadcb26b4f
pythonicforge <pythonicforge@gmail.com>
2024-06-23 11:52:55 +0530

    Added: 404 🌐

--- routes.py
+++ routes.py
@@ -5,3 +5,10 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def main():
     return render_template('base.html')
+
+@main_bp.route('/<section>')
+def section(section):
+    try:
+        return render_template(f'{section}.html')
+    except:
+        return render_template("404.html")
